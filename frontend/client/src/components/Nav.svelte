b1d74cc5fb6d46a16dc92286026e7ba6076c2daa
Steve Giordano <stephenagiordano@gmail.com>
2024-01-21 21:53:58 -0500

    added page routing

--- frontend/client/src/components/Nav.svelte
+++ frontend/client/src/components/Nav.svelte
@@ -1,4 +1,19 @@
-<script>
+<script lang="ts">
+	import page from "page";
+	import Home from "../pages/Home.svelte";
+	import Account from "../pages/Account.svelte";
+	import About from "../pages/About.svelte";
+	import Error from "../pages/Error.svelte";
+
+	let current = Home;
+
+	page("/", () => (current = Home));
+	page("/about", () => (current = About));
+	page("/account", () => (current = Account));
+	page("*", () => (current = Error));
+
+	page.start();
+
 </script>
 
 <div id="nav">
@@ -7,6 +22,7 @@
 		<a class="navbar-item" href="/about">about</a>
 		<a class="navbar-item" href="/account">account</a>
 	</nav>
+	<svelte:component this={current} />
 </div>
 
 <style>
