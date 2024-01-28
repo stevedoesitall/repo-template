8c5508cf4e49f0fbd3f974c52a12852d4aaa68ee
Steve Giordano <stephenagiordano@gmail.com>
2024-01-27 20:25:44 -0500

    changed lots of deployment stuff

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: chmod +x start.sh && ./start.sh
-release: sh -c 'if [ "$APP_TO_START" = "backend" ]; then cd backend && npm run migrate:prod; fi'
--- start.sh
+++ start.sh
@@ -5,7 +5,9 @@ APP_TO_START=${APP_TO_START:-"backend"}
 if [ "$APP_TO_START" == "frontend" ]; then
   echo "Starting frontend..."
   npm run start:frontend
-else
+elif [ "$APP_TO_START" == "backend" ]; then
   echo "Starting backend..."
   npm run start:backend
+elif [ "$APP_TO_START" == "databse" ]; then
+  echo "Running database migration"
 fi
